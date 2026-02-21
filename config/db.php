444d3e43fa7e11fc5445e5f9b43f39023892fe8f
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-02-21 17:56:48 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -55,3 +55,115 @@ class Database {
             die("Database connection failed. Please try again later.");
         }
     }
+    
+    /**
+     * Get database instance (Singleton pattern)
+     * 
+     * @return Database
+     */
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
+    /**
+     * Get PDO connection
+     * 
+     * @return PDO
+     */
+    public function getConnection() {
+        return $this->connection;
+    }
+}
+
+/**
+ * Get database connection
+ * 
+ * @return PDO
+ */
+function getDB() {
+    return Database::getInstance()->getConnection();
+}
+
+/**
+ * Execute a prepared query
+ * 
+ * @param string $sql SQL query with placeholders
+ * @param array $params Parameters to bind
+ * @return PDOStatement
+ */
+function executeQuery($sql, $params = []) {
+    $db = getDB();
+    $stmt = $db->prepare($sql);
+    $stmt->execute($params);
+    return $stmt;
+}
+
+/**
+ * Fetch single row
+ * 
+ * @param string $sql SQL query
+ * @param array $params Parameters
+ * @return array|false
+ */
+function fetchOne($sql, $params = []) {
+    $stmt = executeQuery($sql, $params);
+    return $stmt->fetch();
+}
+
+/**
+ * Fetch all rows
+ * 
+ * @param string $sql SQL query
+ * @param array $params Parameters
+ * @return array
+ */
+function fetchAll($sql, $params = []) {
+    $stmt = executeQuery($sql, $params);
+    return $stmt->fetchAll();
+}
+
+/**
+ * Insert data and return last insert ID
+ * 
+ * @param string $table Table name
+ * @param array $data Associative array of column => value
+ * @return int Last insert ID
+ */
+function insert($table, $data) {
+    $db = getDB();
+    $columns = implode(', ', array_keys($data));
+    $placeholders = ':' . implode(', :', array_keys($data));
+    
+    $sql = "INSERT INTO {$table} ({$columns}) VALUES ({$placeholders})";
+    $stmt = $db->prepare($sql);
+    $stmt->execute($data);
+    
+    return $db->lastInsertId();
+}
+
+/**
+ * Update data in table
+ * 
+ * @param string $table Table name
+ * @param array $data Associative array of column => value
+ * @param string $where Where clause
+ * @param array $whereParams Where parameters
+ * @return int Affected rows
+ */
+function update($table, $data, $where, $whereParams = []) {
+    $db = getDB();
+    $setParts = [];
+    foreach ($data as $key => $value) {
+        $setParts[] = "{$key} = :{$key}";
+    }
+    $setClause = implode(', ', $setParts);
+    
+    $sql = "UPDATE {$table} SET {$setClause} WHERE {$where}";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array_merge($data, $whereParams));
+    
+    return $stmt->rowCount();
+}
