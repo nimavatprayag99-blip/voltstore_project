bcf74b0ed7227dfafdef67bef6728193cabfc472
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-02-21 17:57:31 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -193,3 +193,34 @@ function sanitize($data) {
     }
     return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
 }
+/**
+ * Generate CSRF token
+ * 
+ * @return string
+ */
+function generateCSRFToken() {
+    if (!isset($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+/**
+ * Verify CSRF token
+ * 
+ * @param string $token Token to verify
+ * @return bool
+ */
+function verifyCSRFToken($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+
+/**
+ * Format price with currency
+ * 
+ * @param float $price Price value
+ * @return string Formatted price
+ */
+function formatPrice($price) {
+    return CURRENCY . number_format($price, 0);
+}
