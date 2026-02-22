3439a9b1af2b5d86ae8f09dc657e2b2c42b12561
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-02-22 14:07:47 +0530

    Update voltstore.sql

---
+++ database/voltstore.sql
@@ -0,0 +1,7 @@
+-- VoltStore E-Commerce Database
+-- Version: 1.0
+-- Description: Complete database schema for VoltStore E-Commerce Platform
+
+-- Create Database
+CREATE DATABASE IF NOT EXISTS voltstore CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE voltstore;
