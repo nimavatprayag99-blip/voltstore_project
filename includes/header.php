1118dc2cb608709dc306d31556b39868a5328660
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-02-23 20:47:44 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * VoltStore - Header Template
+ * 
+ * @package VoltStore
+ * @version 1.0
+ */
+
+// Ensure config is loaded
+if (!defined('SITE_NAME')) {
+    require_once __DIR__ . '/../config/db.php';
+}
