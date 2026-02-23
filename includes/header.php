eb3516f5f2019eaa5c5c4fb4e246c9c1d1059227
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-02-23 20:48:16 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -10,3 +10,11 @@
 if (!defined('SITE_NAME')) {
     require_once __DIR__ . '/../config/db.php';
 }
+require_once __DIR__ . '/functions.php';
+
+// Get cart count if user is logged in
+$cartCount = isLoggedIn() ? getCartCount($_SESSION['user_id']) : 0;
+
+// Get current page for active state
+$currentPage = basename($_SERVER['PHP_SELF'], '.php');
+?>
