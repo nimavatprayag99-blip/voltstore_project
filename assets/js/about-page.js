3c7a3fe40beba530d2dd1b31e608ba23e8d34fcd
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-03-06 18:58:40 +0530

    Update about-page.js

--- assets/js/about-page.js
+++ assets/js/about-page.js
@@ -25,3 +25,20 @@
                         animateCounter(entry.target);
                     }
                 }, parseInt(delay));
+                
+                // Stop observing once animated
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    // Observe all animated elements
+    function observeElements() {
+        const animatedElements = document.querySelectorAll(
+            '.fade-in-up, .fade-in-left, .fade-in-right'
+        );
+
+        animatedElements.forEach(el => {
+            observer.observe(el);
+        });
+    }
