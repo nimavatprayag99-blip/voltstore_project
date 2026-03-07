c1cb5c8a786052b55838a62cf8867bdae4b58aca
PrayagNimavat99 <nimavatprayag99@gmail.com>
2026-03-07 10:13:01 +0530

    products.php

--- products.php
+++ products.php
@@ -328,3 +328,37 @@ include __DIR__ . '/includes/header.php';
                     </div>
                     <?php endforeach; ?>
                 </div>
+                
+                <!-- Pagination -->
+                <?php if ($totalPages > 1): ?>
+                <div class="pagination" style="margin-top: 48px;">
+                    <?php if ($page > 1): ?>
+                    <a href="<?php echo SITE_URL; ?>/products.php?page=<?php echo $page - 1; ?><?php echo $category ? '&category=' . $category : ''; ?><?php echo $search ? '&search=' . urlencode($search) : ''; ?><?php echo $sort !== 'newest' ? '&sort=' . $sort : ''; ?>">
+                        <i class="fas fa-chevron-left"></i>
+                    </a>
+                    <?php endif; ?>
+                    
+                    <?php for ($i = max(1, $page - 2); $i <= min($totalPages, $page + 2); $i++): ?>
+                    <?php if ($i == $page): ?>
+                    <span class="current"><?php echo $i; ?></span>
+                    <?php else: ?>
+                    <a href="<?php echo SITE_URL; ?>/products.php?page=<?php echo $i; ?><?php echo $category ? '&category=' . $category : ''; ?><?php echo $search ? '&search=' . urlencode($search) : ''; ?><?php echo $sort !== 'newest' ? '&sort=' . $sort : ''; ?>">
+                        <?php echo $i; ?>
+                    </a>
+                    <?php endif; ?>
+                    <?php endfor; ?>
+                    
+                    <?php if ($page < $totalPages): ?>
+                    <a href="<?php echo SITE_URL; ?>/products.php?page=<?php echo $page + 1; ?><?php echo $category ? '&category=' . $category : ''; ?><?php echo $search ? '&search=' . urlencode($search) : ''; ?><?php echo $sort !== 'newest' ? '&sort=' . $sort : ''; ?>">
+                        <i class="fas fa-chevron-right"></i>
+                    </a>
+                    <?php endif; ?>
+                </div>
+                <?php endif; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include __DIR__ . '/includes/footer.php'; ?>
